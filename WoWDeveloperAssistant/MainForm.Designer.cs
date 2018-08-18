38e6a0214a2b4540bb9935eeba2d87748ed6f356
DESKTOP-L88SM2N\Pveska <mr.trollun@mail.ru>
2018-08-19 02:03:20 +0300

    Fix form name

--- WoWDeveloperAssistant/MainForm.Designer.cs
+++ WoWDeveloperAssistant/MainForm.Designer.cs
@@ -69,9 +69,9 @@
             this.tabPage_Output.SuspendLayout();
             this.statusStrip_LoadedFile.SuspendLayout();
             this.SuspendLayout();
-            //
+            // 
             // tabControl
-            //
+            // 
             this.tabControl.Controls.Add(this.tabPage_CreatureSpellsCreator);
             this.tabControl.Controls.Add(this.tabPage_Output);
             this.tabControl.Location = new System.Drawing.Point(3, 3);
@@ -79,9 +79,9 @@
             this.tabControl.SelectedIndex = 0;
             this.tabControl.Size = new System.Drawing.Size(1773, 992);
             this.tabControl.TabIndex = 1;
-            //
+            // 
             // tabPage_CreatureSpellsCreator
-            //
+            // 
             this.tabPage_CreatureSpellsCreator.Controls.Add(this.checkBox_OnlyCombatSpells);
             this.tabPage_CreatureSpellsCreator.Controls.Add(this.dataGridView_Spells);
             this.tabPage_CreatureSpellsCreator.Controls.Add(this.listBox_CreatureGuids);
@@ -93,9 +93,9 @@
             this.tabPage_CreatureSpellsCreator.TabIndex = 0;
             this.tabPage_CreatureSpellsCreator.Text = "Creature Spells Creator";
             this.tabPage_CreatureSpellsCreator.UseVisualStyleBackColor = true;
-            //
+            // 
             // checkBox_OnlyCombatSpells
-            //
+            // 
             this.checkBox_OnlyCombatSpells.AutoSize = true;
             this.checkBox_OnlyCombatSpells.Checked = true;
             this.checkBox_OnlyCombatSpells.CheckState = System.Windows.Forms.CheckState.Checked;
@@ -105,9 +105,9 @@
             this.checkBox_OnlyCombatSpells.TabIndex = 4;
             this.checkBox_OnlyCombatSpells.Text = "Only Combat Spells";
             this.checkBox_OnlyCombatSpells.UseVisualStyleBackColor = true;
-            //
+            // 
             // dataGridView_Spells
-            //
+            // 
             this.dataGridView_Spells.AllowUserToAddRows = false;
             this.dataGridView_Spells.AllowUserToDeleteRows = false;
             this.dataGridView_Spells.AllowUserToOrderColumns = true;
@@ -133,9 +133,9 @@
             this.dataGridView_Spells.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView_Spells.Size = new System.Drawing.Size(1379, 904);
             this.dataGridView_Spells.TabIndex = 3;
-            //
+            // 
             // Spell_Id
-            //
+            // 
             this.Spell_Id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             this.Spell_Id.DefaultCellStyle = dataGridViewCellStyle1;
@@ -144,9 +144,9 @@
             this.Spell_Id.Name = "Spell_Id";
             this.Spell_Id.ReadOnly = true;
             this.Spell_Id.Width = 103;
-            //
+            // 
             // Spell_Name
-            //
+            // 
             this.Spell_Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             this.Spell_Name.DefaultCellStyle = dataGridViewCellStyle2;
@@ -154,9 +154,9 @@
             this.Spell_Name.Name = "Spell_Name";
             this.Spell_Name.ReadOnly = true;
             this.Spell_Name.Width = 131;
-            //
+            // 
             // Cast_Time
-            //
+            // 
             this.Cast_Time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             this.Cast_Time.DefaultCellStyle = dataGridViewCellStyle3;
@@ -164,41 +164,41 @@
             this.Cast_Time.Name = "Cast_Time";
             this.Cast_Time.ReadOnly = true;
             this.Cast_Time.Width = 121;
-            //
+            // 
             // Min_Cast_Start_Time
-            //
+            // 
             this.Min_Cast_Start_Time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
             this.Min_Cast_Start_Time.HeaderText = "Min_Cast_Start_Time";
             this.Min_Cast_Start_Time.Name = "Min_Cast_Start_Time";
             this.Min_Cast_Start_Time.ReadOnly = true;
             this.Min_Cast_Start_Time.Width = 199;
-            //
+            // 
             // Max_Cast_Start_Time
-            //
+            // 
             this.Max_Cast_Start_Time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
             this.Max_Cast_Start_Time.HeaderText = "Max_Cast_Start_Time";
             this.Max_Cast_Start_Time.Name = "Max_Cast_Start_Time";
             this.Max_Cast_Start_Time.ReadOnly = true;
             this.Max_Cast_Start_Time.Width = 203;
-            //
+            // 
             // Min_Cast_Repeat_Time
-            //
+            // 
             this.Min_Cast_Repeat_Time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
             this.Min_Cast_Repeat_Time.HeaderText = "Min_Cast_Repeat_Time";
             this.Min_Cast_Repeat_Time.Name = "Min_Cast_Repeat_Time";
             this.Min_Cast_Repeat_Time.ReadOnly = true;
             this.Min_Cast_Repeat_Time.Width = 217;
-            //
+            // 
             // Max_Cast_Repeat_Time
-            //
+            // 
             this.Max_Cast_Repeat_Time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
             this.Max_Cast_Repeat_Time.HeaderText = "Max_Cast_Repeat_Time";
             this.Max_Cast_Repeat_Time.Name = "Max_Cast_Repeat_Time";
             this.Max_Cast_Repeat_Time.ReadOnly = true;
             this.Max_Cast_Repeat_Time.Width = 221;
-            //
+            // 
             // Casts_Count
-            //
+            // 
             this.Casts_Count.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
             dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             this.Casts_Count.DefaultCellStyle = dataGridViewCellStyle4;
@@ -206,9 +206,9 @@
             this.Casts_Count.Name = "Casts_Count";
             this.Casts_Count.ReadOnly = true;
             this.Casts_Count.Width = 138;
-            //
+            // 
             // contextMenuStrip
-            //
+            // 
             this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.cutToolStripMenuItem,
@@ -216,28 +216,28 @@
             this.createSQLToolStripMenuItem});
             this.contextMenuStrip.Name = "contextMenuStrip1";
             this.contextMenuStrip.Size = new System.Drawing.Size(172, 70);
-            //
+            // 
             // cutToolStripMenuItem
-            //
+            // 
             this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
             this.cutToolStripMenuItem.Size = new System.Drawing.Size(171, 30);
             this.cutToolStripMenuItem.Text = "Cut";
             this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
-            //
+            // 
             // toolStripSeparator
-            //
+            // 
             this.toolStripSeparator.Name = "toolStripSeparator";
             this.toolStripSeparator.Size = new System.Drawing.Size(168, 6);
-            //
+            // 
             // createSQLToolStripMenuItem
-            //
+            // 
             this.createSQLToolStripMenuItem.Name = "createSQLToolStripMenuItem";
             this.createSQLToolStripMenuItem.Size = new System.Drawing.Size(171, 30);
             this.createSQLToolStripMenuItem.Text = "Create SQL";
             this.createSQLToolStripMenuItem.Click += new System.EventHandler(this.createSQLToolStripMenuItem_Click);
-            //
+            // 
             // listBox_CreatureGuids
-            //
+            // 
             this.listBox_CreatureGuids.Enabled = false;
             this.listBox_CreatureGuids.FormattingEnabled = true;
             this.listBox_CreatureGuids.ItemHeight = 20;
@@ -246,9 +246,9 @@
             this.listBox_CreatureGuids.Size = new System.Drawing.Size(356, 904);
             this.listBox_CreatureGuids.TabIndex = 2;
             this.listBox_CreatureGuids.SelectedIndexChanged += new System.EventHandler(this.listBox_CreatureGuids_SelectedIndexChanged);
-            //
+            // 
             // toolStrip_CreatureSpellsCreator
-            //
+            // 
             this.toolStrip_CreatureSpellsCreator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton_ImportSniff,
             this.toolStripButton_Search,
@@ -259,45 +259,44 @@
             this.toolStrip_CreatureSpellsCreator.Size = new System.Drawing.Size(1759, 32);
             this.toolStrip_CreatureSpellsCreator.TabIndex = 1;
             this.toolStrip_CreatureSpellsCreator.Text = "toolStrip_CreatureSpellsCreator";
-            this.toolStrip_CreatureSpellsCreator.ImageScalingSize = new System.Drawing.Size(16, 16);
-            //
+            // 
             // toolStripButton_ImportSniff
-            //
+            // 
             this.toolStripButton_ImportSniff.Image = global::WoWDeveloperAssistant.Properties.Resources.PIC_Import;
             this.toolStripButton_ImportSniff.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_ImportSniff.Name = "toolStripButton_ImportSniff";
-            this.toolStripButton_ImportSniff.Size = new System.Drawing.Size(136, 29);
+            this.toolStripButton_ImportSniff.Size = new System.Drawing.Size(128, 29);
             this.toolStripButton_ImportSniff.Text = "Import Sniff";
             this.toolStripButton_ImportSniff.Click += new System.EventHandler(this.toolStripButton_ImportSniff_Click);
-            //
+            // 
             // toolStripButton_Search
-            //
+            // 
             this.toolStripButton_Search.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.toolStripButton_Search.Enabled = false;
             this.toolStripButton_Search.Image = global::WoWDeveloperAssistant.Properties.Resources.PIC_Search;
             this.toolStripButton_Search.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_Search.Name = "toolStripButton_Search";
-            this.toolStripButton_Search.Size = new System.Drawing.Size(92, 29);
+            this.toolStripButton_Search.Size = new System.Drawing.Size(84, 29);
             this.toolStripButton_Search.Text = "Search";
             this.toolStripButton_Search.Click += new System.EventHandler(this.toolStripButton_Search_Click);
-            //
+            // 
             // toolStripTextBox_CreatureEntry
-            //
+            // 
             this.toolStripTextBox_CreatureEntry.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.toolStripTextBox_CreatureEntry.Enabled = false;
             this.toolStripTextBox_CreatureEntry.MaxLength = 10;
             this.toolStripTextBox_CreatureEntry.Name = "toolStripTextBox_CreatureEntry";
             this.toolStripTextBox_CreatureEntry.Size = new System.Drawing.Size(100, 32);
-            //
+            // 
             // toolStripLabel_CreatureEntry
-            //
+            // 
             this.toolStripLabel_CreatureEntry.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.toolStripLabel_CreatureEntry.Name = "toolStripLabel_CreatureEntry";
             this.toolStripLabel_CreatureEntry.Size = new System.Drawing.Size(127, 29);
             this.toolStripLabel_CreatureEntry.Text = "Creature Entry:";
-            //
+            // 
             // tabPage_Output
-            //
+            // 
             this.tabPage_Output.Controls.Add(this.textBox_SQLOutput);
             this.tabPage_Output.Location = new System.Drawing.Point(4, 29);
             this.tabPage_Output.Name = "tabPage_Output";
@@ -306,9 +305,9 @@
             this.tabPage_Output.TabIndex = 1;
             this.tabPage_Output.Text = "SQL Output";
             this.tabPage_Output.UseVisualStyleBackColor = true;
-            //
+            // 
             // textBox_SQLOutput
-            //
+            // 
             this.textBox_SQLOutput.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox_SQLOutput.Font = new System.Drawing.Font("Courier New", 9.75F);
             this.textBox_SQLOutput.Location = new System.Drawing.Point(3, 3);
@@ -318,9 +317,9 @@
             this.textBox_SQLOutput.Size = new System.Drawing.Size(1759, 953);
             this.textBox_SQLOutput.TabIndex = 0;
             this.textBox_SQLOutput.WordWrap = false;
-            //
+            // 
             // statusStrip_LoadedFile
-            //
+            // 
             this.statusStrip_LoadedFile.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.statusStrip_LoadedFile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel_FileStatus});
@@ -329,19 +328,19 @@
             this.statusStrip_LoadedFile.Size = new System.Drawing.Size(1776, 30);
             this.statusStrip_LoadedFile.TabIndex = 2;
             this.statusStrip_LoadedFile.Text = "statusStrip";
-            //
+            // 
             // toolStripStatusLabel_FileStatus
-            //
+            // 
             this.toolStripStatusLabel_FileStatus.Name = "toolStripStatusLabel_FileStatus";
             this.toolStripStatusLabel_FileStatus.Size = new System.Drawing.Size(131, 25);
             this.toolStripStatusLabel_FileStatus.Text = "No File Loaded";
-            //
+            // 
             // openFileDialog
-            //
+            // 
             this.openFileDialog.FileName = "openFileDialog";
-            //
+            // 
             // MainForm
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1776, 1027);
@@ -350,7 +349,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.Text = "Wow Developer Assistance";
+            this.Text = "Wow Developer Assistant";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.tabControl.ResumeLayout(false);
             this.tabPage_CreatureSpellsCreator.ResumeLayout(false);
