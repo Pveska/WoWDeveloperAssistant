9f7bd75c08ca26786c01cb87af15a03fe52c65fa
Pveska <25360098+Pveska@users.noreply.github.com>
2021-12-18 13:05:12 +0300

    Modify GossipMenuAdvisor so it can work with multiple texts

--- WoWDeveloperAssistant/Database Advisor/GossipMenuAdvisor.cs
+++ WoWDeveloperAssistant/Database Advisor/GossipMenuAdvisor.cs
@@ -27,14 +27,46 @@ namespace WoWDeveloperAssistant.Database_Advisor
                 {
                     output += "Text Id: " + gossipMenuRow[0].ToString() + "\r\n";
 
-                    DataSet npcTextBroadcastIdDs = SQLModule.DatabaseSelectQuery("SELECT `BroadcastTextID0` FROM `npc_text` WHERE `ID` = " + gossipMenuRow[0].ToString() + ";");
+                    DataSet npcTextBroadcastIdDs = SQLModule.DatabaseSelectQuery("SELECT `BroadcastTextID0`, `BroadcastTextID1`, `BroadcastTextID2`, `BroadcastTextID3`, `BroadcastTextID4`, `BroadcastTextID5`, `BroadcastTextID6`, `BroadcastTextID7` FROM `npc_text` WHERE `ID` = " + gossipMenuRow[0].ToString() + ";");
                     if (npcTextBroadcastIdDs == null || npcTextBroadcastIdDs.Tables["table"].Rows.Count == 0)
                     {
                         output += "There is no npc text with this TextId in your database!";
                         continue;
                     }
 
-                    DataSet broadcastTextDs = SQLModule.HotfixSelectQuery("SELECT `Text`, `Text1` FROM `broadcasttext` WHERE `ROW_ID` = " + npcTextBroadcastIdDs.Tables["table"].Rows[0][0].ToString() + ";");
+                    foreach (var itr in npcTextBroadcastIdDs.Tables["table"].Rows[0].ItemArray.Where(x => x.ToString() != "0"))
+                    {
+                        DataSet broadcastTextDs = SQLModule.HotfixSelectQuery("SELECT `Text`, `Text1` FROM `broadcasttext` WHERE `ROW_ID` = " + itr.ToString() + ";");
+                        if (broadcastTextDs == null || broadcastTextDs.Tables["table"].Rows.Count == 0)
+                        {
+                            output += "There is no broadcast text with this BroadcastId in your database!";
+                            continue;
+                        }
+
+                        foreach (string stringRow in broadcastTextDs.Tables["table"].Rows[0].ItemArray)
+                        {
+                            if (stringRow != "")
+                            {
+                                output += "- " + stringRow + "\r\n";
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                output += "Text Id: " + gossipMenuTextIdDs.Tables["table"].Rows[0][0].ToString() + "\r\n";
+
+                DataSet npcTextBroadcastIdDs = SQLModule.DatabaseSelectQuery("SELECT `BroadcastTextID0`, `BroadcastTextID1`, `BroadcastTextID2`, `BroadcastTextID3`, `BroadcastTextID4`, `BroadcastTextID5`, `BroadcastTextID6`, `BroadcastTextID7` FROM `npc_text` WHERE `ID` = " + gossipMenuTextIdDs.Tables["table"].Rows[0][0].ToString() + ";");
+                if (npcTextBroadcastIdDs == null || npcTextBroadcastIdDs.Tables["table"].Rows.Count == 0)
+                {
+                    MessageBox.Show("There is no npc text with this TextId in your database!");
+                    return output;
+                }
+
+                foreach (var itr in npcTextBroadcastIdDs.Tables["table"].Rows[0].ItemArray.Where(x => x.ToString() != "0"))
+                {
+                    DataSet broadcastTextDs = SQLModule.HotfixSelectQuery("SELECT `Text`, `Text1` FROM `broadcasttext` WHERE `ROW_ID` = " + itr.ToString() + ";");
                     if (broadcastTextDs == null || broadcastTextDs.Tables["table"].Rows.Count == 0)
                     {
                         output += "There is no broadcast text with this BroadcastId in your database!";
@@ -45,36 +77,8 @@ namespace WoWDeveloperAssistant.Database_Advisor
                     {
                         if (stringRow != "")
                         {
-                            output += stringRow + "\r\n";
+                            output += "- " + stringRow + "\r\n";
                         }
-                    }
-
-                    output += "\r\n";
-                }
-            }
-            else
-            {
-                output += "Text Id: " + gossipMenuTextIdDs.Tables["table"].Rows[0][0].ToString() + "\r\n";
-
-                DataSet npcTextBroadcastIdDs = SQLModule.DatabaseSelectQuery("SELECT `BroadcastTextID0` FROM `npc_text` WHERE `ID` = " + gossipMenuTextIdDs.Tables["table"].Rows[0][0].ToString() + ";");
-                if (npcTextBroadcastIdDs == null || npcTextBroadcastIdDs.Tables["table"].Rows.Count == 0)
-                {
-                    MessageBox.Show("There is no npc text with this TextId in your database!");
-                    return output;
-                }
-
-                DataSet broadcastTextDs = SQLModule.HotfixSelectQuery("SELECT `Text`, `Text1` FROM `broadcasttext` WHERE `ROW_ID` = " + npcTextBroadcastIdDs.Tables["table"].Rows[0][0].ToString() + ";");
-                if (broadcastTextDs == null || broadcastTextDs.Tables["table"].Rows.Count == 0)
-                {
-                    MessageBox.Show("There is no broadcast text with this Id in your database!");
-                    return output;
-                }
-
-                foreach (string textRow in broadcastTextDs.Tables["table"].Rows[0].ItemArray)
-                {
-                    if (textRow != "")
-                    {
-                        output += textRow + "\r\n";
                     }
                 }
             }
